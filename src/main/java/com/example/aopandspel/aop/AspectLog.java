5330480209d76b45f12ff55e9212bd8afd581526
Dmitriy Shumskiy <dmitriyshum2292@gmail.com>
2021-04-22 09:42:43 +0500

    added two AOP methods

--- src/main/java/com/example/aopandspel/aop/AspectLog.java
+++ src/main/java/com/example/aopandspel/aop/AspectLog.java
@@ -1,11 +1,11 @@
 package com.example.aopandspel.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,5 +22,29 @@ public class AspectLog {
         StringBuilder message = new StringBuilder("Message: method ");
         message.append(joinPoint.getSignature().getName());
         logger.error(message.toString());
+    }
+    @AfterReturning(pointcut = "executeLogging()",returning = "returnValue")
+    public void logMethodCallWithReturnValue(JoinPoint joinPoint,Object returnValue){
+        Logger logger = LoggerFactory.getLogger(AspectLog.class);
+        StringBuilder message = new StringBuilder("Another Message: method ");
+        message.append(joinPoint.getSignature().getName());
+        logger.info(message.toString());
+    }
+    @Around("executeLogging()")
+    public Object aroundLogMethodCall(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        Logger logger = LoggerFactory.getLogger(AspectLog.class);
+
+        long startTime = System.currentTimeMillis();
+
+        Object returnValue = joinPoint.proceed();
+
+        StringBuilder message = new StringBuilder("With Time: method ");
+        message.append(joinPoint.getSignature().getName());
+
+        long spendTime = System.currentTimeMillis()-startTime;
+        message.append(" time: "+spendTime+" ms");
+        logger.info(message.toString());
+        return returnValue;
     }
 }
